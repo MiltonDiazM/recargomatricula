13e37323f9e2ed2f0e0925119cff5c1c41dc28bb
Milton Diaz <diazmorcillomilton@gmail.com>
2021-06-18 07:17:54 -0500

    Enunciado del ejercicio

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+//se ingresa la edad de un estudiante si la edad es de 5 a 10 años se hace
+//un recargo del 1% al costo de la matricula. Si la edad es superior a 10 años
+//y menor o igual que 15 años el recargo sera del 2%. Si la edad es mayor que
+//15 y menor o igual que 20, el recargo es del 4%. Si la edad es mayor que
+//a 20 años, el recargo es del 6%
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
